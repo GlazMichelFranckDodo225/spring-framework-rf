5ea4d40988614038b018870c7b7f3c8c7659e5e5
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-17 15:56:46 +0200

    Constructor Based Dependency Injection

--- src/main/java/com/dgmf/withspringdependencyinjection/MessageSender.java
+++ src/main/java/com/dgmf/withspringdependencyinjection/MessageSender.java
@@ -8,14 +8,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageSender {
     private MessageService messageService;
+    private MessageService smsService;
 
-    @Autowired
-    public MessageSender(@Qualifier("emailService") MessageService messageService) {
+    // @Autowired // Constructor-based Dependency Injection
+    public MessageSender(
+            @Qualifier("emailService") MessageService messageService) {
         this.messageService = messageService;
+
+        System.out.println("Constructor-based Dependency Injection");
+    }
+
+    @Autowired // Constructor-based Dependency Injection
+    public MessageSender(
+            @Qualifier("emailService") MessageService messageService,
+            MessageService smsService
+            ) {
+        this.messageService = messageService;
+        this.smsService = smsService;
+
+        System.out.println("Constructor-based Dependency Injection 2");
     }
 
     public void sendMessage(String message) {
         System.out.println("MessageSender Class");
         messageService.sendMessage(message);
+        smsService.sendMessage(message);
     }
 }
